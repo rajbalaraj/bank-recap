6a7039343a310bbd7220d40832cc7455b2ed9a4b
rajbalaraj <rajbalasr15@gmail.com>
2022-12-30 02:26:45 +0600

    set-rules

---
+++ js/deposit.js
@@ -0,0 +1,10 @@
+/* 
+1. add event listener to the deposit button
+2. get deposit amount from the deposit input field
+2-5. Convert string deposit amount to a number type
+3. clear the deposit input field after getting the value
+4. get the previous deposit total 
+5. calculate new deposit total and set the value to the deposit total
+6. get current balance total
+7. calculate the new balance and set it to the balance total element
+*/
