255bf73608ee40bd0d314811ff42eb6738629926
rajbalaraj <rajbalasr15@gmail.com>
2022-12-30 02:29:32 +0600

    setup-rules

---
+++ js/withdraw.js
@@ -0,0 +1,9 @@
+/* 
+1. add event handler with the withdraw button
+2. get the withdraw amount
+3. clear the withdraw input field
+4. get previous withdraw total
+5. calculate total Withdraw amount and set it to the withdraw total element
+6. get previous balance
+7. calculate new balance and set it to the balance total element
+*/
