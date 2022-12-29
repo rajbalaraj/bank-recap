a56b0b3e0be226eaa7fbd70c03b88f59a59cc80c
rajbalaraj <rajbalasr15@gmail.com>
2022-12-30 02:30:42 +0600

    withdraw-js

--- js/withdraw.js
+++ js/withdraw.js
@@ -7,3 +7,31 @@
 6. get previous balance
 7. calculate new balance and set it to the balance total element
 */
+// step-1
+document.getElementById('btn-withdraw').addEventListener('click', function () {
+    // step-2
+    const withdrawField = document.getElementById('withdraw-field');
+    const newWithdrawAmountString = withdrawField.value;
+    const newWithdrawAmount = parseFloat(newWithdrawAmountString);
+
+    // step-3
+    withdrawField.value = '';
+
+    // step-4
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previousWithdrawTotalString = withdrawTotalElement.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
+
+    // step-5
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+    withdrawTotalElement.innerText = newWithdrawTotal;
+
+    // step-6
+    const balanceElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // step-7
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceElement.innerText = newBalanceTotal;
+})
